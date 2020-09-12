1ad6c05a6f5e31db884a24d12055504fdbba5fdb
unknown <jpar303@gmail.com>
2020-09-13 01:28:10 +0900

    haha

---
+++ bnb-clone/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Home from './Home';
+import Header from './Header';
+import './App.css';
+
+function App() {
+  return (
+    
+    <div className="app">
+      <Header />
+    
+      <Home />
+    </div>
+  );
+
+
+}
+
+export default App;
---
+++ bnb-clone/src/Home.js
@@ -0,0 +1,16 @@
+//https://www.youtube.com/watch?v=BtJeH_-XYaA
+//for my personal react training
+
+import React from 'react';
+import './Home.css'
+import Banner from './Banner';
+
+function Home() {
+    return (
+        <div className='home'>
+            <Banner />
+        </div>
+    )
+}
+
+export default Home;
