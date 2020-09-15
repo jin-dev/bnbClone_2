87b0abe24cd9c23db8a48132d5413db685e109f9
jin <jin@jinui-iMac.local>
2020-09-15 15:21:21 +0900

    completed demo clone

--- bnb-clone/src/Banner.js
+++ bnb-clone/src/Banner.js
@@ -2,9 +2,11 @@ import React, { useState} from 'react';
 import './Banner.css';
 import { Button } from '@material-ui/core'
 import Search from './Search'
+import { useHistory } from 'react-router-dom';
 function Banner() {
 
-        const [showSearch, setShowSearch] = useState(false);
+    const history = useHistory();
+    const [showSearch, setShowSearch] = useState(false);
     
 
     return (
@@ -16,7 +18,7 @@ function Banner() {
             <div className="banner_info">
                 <h1> Get out and stretch your imagination</h1>
                 <h5> Plan a different kind of get away to uncover the hidden gems near you</h5>
-                <Button variant='outlined'>Explore Nearby</Button>
+                <Button onClick={() => history.push('/search')} variant='outlined'>Explore Nearby</Button>
             </div>
             
         </div>
